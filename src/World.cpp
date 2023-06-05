857e00dacca9c819955ed71210e8e57ab53ec531
BlackyDrum <ganiaytan1010@web.de>
2023-06-05 12:25:28 +0200

    Introduce variables for offest when moving the ground

--- src/World.cpp
+++ src/World.cpp
@@ -20,20 +20,23 @@ bool World::loadAssets()
 void World::moveGround()
 {
 	/*
-	* Initialize starting position for the two ground sprites in the std::pair
+	* Move the two ground sprites to the left side of the screen
 	*/
 	m_ground.first.setPosition(m_ground.first.getPosition().x - m_moveSpeed,m_ground.first.getPosition().y);
 	m_ground.second.setPosition(sf::Vector2f(m_ground.second.getPosition().x - m_moveSpeed,m_ground.second.getPosition().y));
+
+	int offsetFirst = int(m_ground.first.getPosition().x) * -1.0 - int(m_groundTexture.getSize().x);
+	int offsetSecond = int(m_ground.second.getPosition().x) * -1.0 - int(m_groundTexture.getSize().x);
 
 	/*
 	* When one of the two ground sprites reaches the left end of the screen, they are immediately teleported back to the right side of the screen
 	* with a little offset, depending if there is a gap between the first and second ground sprite after teleportation
 	*/
 	if (m_ground.first.getPosition().x <= m_groundTexture.getSize().x * -1.0) 
-		m_ground.first.setPosition(SCREEN_WIDTH - (int(m_ground.first.getPosition().x) * -1.0 - int(m_groundTexture.getSize().x)), m_ground.first.getPosition().y);	
+		m_ground.first.setPosition(SCREEN_WIDTH - offsetFirst, m_ground.first.getPosition().y);
 
 	if (m_ground.second.getPosition().x <= m_groundTexture.getSize().x * -1.0)
-		m_ground.second.setPosition(SCREEN_WIDTH - (int(m_ground.second.getPosition().x) * -1.0 - int(m_groundTexture.getSize().x)), m_ground.second.getPosition().y);
+		m_ground.second.setPosition(SCREEN_WIDTH - offsetSecond, m_ground.second.getPosition().y);
 }
 
 void World::changeBackground(int background)
